c4e24f376e8287c7dda41d179601b3ae9b1a86fd
emileporee <158354847+emileporee@users.noreply.github.com>
2024-02-07 09:45:25 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
-class User {
-constructor(name, userid){
-this.Username = name;
-this.UserId = userid;
-  return this
+const Audio1 = new Audio
+let AudioCTX = new AudioContext
+Audio1.src = "laughing.mp3"
+
+function playaudio() {
+Audio1.play()
 }
+
+function stopaudio() {
+Audio1.pause()
 }
-
-let button = document.getElementById("sign+up")
-let B_Piano = document.getElementById("piano")
-let B_Drums = document.getElementById("drums")
-let B_Guitar = document.getElementById("guitar")
-let newUser = null
-
-button.addEventListener("click", () => {
-newUser = new User("TEST_USER", 9249402)
-})
-
-if (newUser ~= null and newUser.Username) { console.log(newUser.Username, newUser.UserId) }
