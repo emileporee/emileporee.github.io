e6f076f77c69e91efb8b7793d66f078f00503019
emileporee <158354847+emileporee@users.noreply.github.com>
2024-03-08 11:04:23 -0800

    Create drawingpage.js

---
+++ drawingpage.js
@@ -0,0 +1,41 @@
+// Write JavaScript Here:
+
+const DrawingBoard = document.getElementById("drawing-board")
+const CTX = DrawingBoard.getContext("2d")
+const Increment = document.getElementById("increment")
+const Decrement = document.getElementById("decrement")
+const Display = document.getElementById("display")
+const Clear = document.getElementById("clear")
+let LINE_SIZE = 10
+let isDrawing = false
+
+DrawingBoard.addEventListener("mouseup", () => {
+    isDrawing = false
+})
+DrawingBoard.addEventListener("mousedown", () => {
+    isDrawing = true
+    CTX.beginPath()
+})
+DrawingBoard.addEventListener("mousemove", (event) => {
+    if (!isDrawing) { return }
+    CTX.lineWidth = LINE_SIZE
+    CTX.lineCap = "round"
+    CTX.lineTo(event.clientX, event.clientY)
+    CTX.stroke()
+    CTX.beginPath()
+    CTX.moveTo(event.clientX, event.clientY)
+})
+
+Increment.addEventListener("click", () => {
+    if ((LINE_SIZE + 1) === 25) { return }
+    LINE_SIZE += 1
+    Display.innerText = LINE_SIZE
+})
+Decrement.addEventListener("click", () => {
+    if ((LINE_SIZE - 1) === 0) { return }
+    LINE_SIZE -= 1
+    Display.innerText = LINE_SIZE
+})
+Clear.addEventListener("click", () => {
+    CTX.clearRect(0, 0, DrawingBoard.width, DrawingBoard.height)
+})
