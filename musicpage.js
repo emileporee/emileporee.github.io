b8bdacf893bf079b524cf33014b9d12e5dd710b8
dxxmed <123851757+dxxmed@users.noreply.github.com>
2024-05-10 17:52:26 -0700

    update 101

--- musicpage.js
+++ musicpage.js
@@ -69,7 +69,7 @@ try {
         numberOfFrames: 5,
         numberOfChannels: 10,
         timestamp: 1000,
-        data: new DataView(new ArrayBuffer(256), 32, 16).setUint32(16, 5)
+        data: new DataView(new ArrayBuffer(256), 32, 16)
     }))
     alert(EncodedData)
 }
